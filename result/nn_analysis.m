5048e4295d9e6ff8b7acb8c5ec128c38ff2023f2
Donghyeon Kim <kdh0429@snu.ac.kr>
2022-05-19 17:20:50 +0900

    Add result analysis matlab file

--- result/nn_analysis.m
+++ result/nn_analysis.m
@@ -19,13 +19,21 @@ sin_phase = sin(2*pi*phase);
 cos_phase = cos(2*pi*phase);
 
 %%
+q(:,24) = 0.0;
+q(:,25) = 0.0;
+qdot(:,24) = 0.0;
+qdot(:,25) = 0.0;
+qdot_lpf(:,24) = 0.0;
+qdot_lpf(:,25) = 0.0;
+
+%%
 obs = [euler_angle, q, qdot, sin_phase, cos_phase];
 obs_lpf = [euler_angle, q, qdot_lpf, sin_phase, cos_phase];
 normalized_obs = obs;
 normalized_obs_lpf = obs_lpf;
 for i=1:size(obs,2)
-    normalized_obs(:,i) = (normalized_obs(:,i) - mean(i)) / sqrt(var(i));
-    normalized_obs_lpf(:,i) = (normalized_obs_lpf(:,i) - mean(i)) / sqrt(var(i));
+    normalized_obs(:,i) = (normalized_obs(:,i) - mean(i)) / sqrt(var(i)+1e-8);
+    normalized_obs_lpf(:,i) = (normalized_obs_lpf(:,i) - mean(i)) / sqrt(var(i)+1e-8);
 end
 
 %%
@@ -35,4 +43,44 @@ for i=1:size(obs,2)
     plot(normalized_obs(:,i))
     hold on
     plot(normalized_obs_lpf(:,i))
+end
+
+%%
+w0 = load('mlp_extractor_policy_net_0_weight.txt');
+b0 = load('mlp_extractor_policy_net_0_bias.txt');
+w2 = load('mlp_extractor_policy_net_2_weight.txt');
+b2 = load('mlp_extractor_policy_net_2_bias.txt');
+w4 = load('action_net_weight.txt');
+b4 = load('action_net_bias.txt');
+
+normalized_obs(normalized_obs < -3.0) = -3.0;
+normalized_obs(normalized_obs > 3.0) = 3.0;
+
+layer0 = normalized_obs*w0' + b0';
+layer0(layer0 < 0.0) = 0.0;
+
+layer2 = layer0*w2' +b2';
+layer2(layer2 < 0.0) = 0.0;
+
+output = layer2*w4' +b4';
+
+
+normalized_obs_lpf(normalized_obs_lpf < -3.0) = -3.0;
+normalized_obs_lpf(normalized_obs_lpf > 3.0) = 3.0;
+
+layer0_lpf = normalized_obs_lpf*w0' + b0';
+layer0_lpf(layer0_lpf < 0.0) = 0.0;
+
+layer2_lpf = layer0_lpf*w2' +b2';
+layer2_lpf(layer2_lpf < 0.0) = 0.0;
+
+output_lpf = layer2_lpf*w4' +b4';
+
+figure()
+for i=1:33
+    subplot(6,6,i)
+    plot(output(:,i))
+    hold on
+    plot(output_lpf(:,i))
+    plot(tau(:,i))
 end
