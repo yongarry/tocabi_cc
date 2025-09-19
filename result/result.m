1cc5fb4e57f402735ef7d1df65a2f342c50f3d48
yongarry <jasonshin0537@snu.ac.kr>
2025-09-19 09:56:59 +0900

    policy update

--- result/result.m
+++ result/result.m
@@ -6,9 +6,9 @@ y = data(:,3);
 yaw = data(:,4);
 
 
-%means_intern = [mean(xy),mean(x),mean(y),mean(yaw)]
-%stds_intern = [std(xy),std(x),std(y),std(yaw)]
-means_heuri = [mean(xy),mean(x),mean(y),mean(yaw)]
-stds_heuri = [std(xy),std(x),std(y),std(yaw)]
+means_intern = [mean(xy),mean(x),mean(y),mean(yaw)]
+stds_intern = [std(xy),std(x),std(y),std(yaw)]
+% means_heuri = [mean(xy),mean(x),mean(y),mean(yaw)]
+% stds_heuri = [std(xy),std(x),std(y),std(yaw)]
 %means_ral = [mean(xy),mean(x),mean(y),mean(yaw)]
 %stds_ral = [std(xy),std(x),std(y),std(yaw)]
